7cfa951536e094a4db92879f893ee324aa3a9823
James Del Bonis III <delbonis.j@husky.neu.edu>
2018-02-02 12:54:18 -0500

    Tweaked the package installation script, fixing most of the weird errors we were having.

--- mods/20-updates.sh
+++ mods/20-updates.sh
@@ -2,16 +2,30 @@
 
 set -x
 
+# We just do this a bunch of times so that we don't have to care about it.
+# FIXME This doesn't really work as well as we want it to.
+function dellocales () {
+	locale-gen --purge en_US.UTF-8
+}
+
 # First, update things
+dellocales
 apt update
+
+# First, make sure we get rid of stupid shit.
+apt -y purge thunderbird
+dpkg -P postfix
 
 # Now, hold the Linux packages (since those won't work anyways) and then upgrade stuff.
 apt-mark hold $(dpkg -l | awk '{ print $2 }' | grep -E '^linux')
+apt-mark locale
 apt -y upgrade
 
 # Now install other packages for us.
-apt -y install $(grep -v "^#" /mnt/packages.list | tr -s '\n' ' ')
+dellocales
+apt -y install $(grep -v "^#" /mnt/scripts/packages.list | tr -s '\n' ' ')
 
-# And some cleanup.
-apy --fix-missing -f install
+# And some cleanup.  This probably won't do all that much.
+apt --fix-missing -f install
 apt -y autoremove
+dellocales
