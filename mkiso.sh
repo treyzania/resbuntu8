a034bfa75ba256d5a90077f14d87739899bc95cf
James Del Bonis III <delbonis.j@husky.neu.edu>
2018-02-07 11:10:19 -0500

    Modified mkiso.sh script to make it actually create the iso.

--- mkiso.sh
+++ mkiso.sh
@@ -5,7 +5,9 @@ workdir=$(realpath $1)
 set -ex
 
 # Rebuild the squashfs filesystem.
-mksquashfs $workdir/rootfs $workdir/isodata/casper/filesystem.squashfs -b 1024k -comp xz -Xbcj x86 -e boot
+rootimgpath=$workdir/isodata/casper/filesystem.squashfs
+rm $rootimgpath
+mksquashfs $workdir/rootfs $rootimgpath -b 1024k -comp xz -Xbcj x86 -e boot
 
 # Actually package it together.
 genisoimage -o resbuntu.iso $workdir/isodata
